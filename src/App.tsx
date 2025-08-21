1192d63bf4b482066fa3cb7eec53c0970c912949
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-08-21 12:11:12 +0500

    Create ChatMessage Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import ChatInput from "./components/ChatInput";
+import ChatMessage from "./components/ChatMessage";
 
 const App = () => {
   return (
     <>
       <ChatInput />
+      <ChatMessage message="hello chatbot" sender="user" />
+      <ChatMessage message="Hello! How can I help you?" sender="robot" />
+      <ChatMessage message="can you get me todays date?" sender="user" />
+      <ChatMessage message="Today is August 21" sender="robot" />
     </>
   );
 };
---
+++ src/components/ChatMessage.tsx
@@ -0,0 +1,19 @@
+import userProfileImage from "../assets/user.png";
+import RobotProfileImage from "../assets/robot.png";
+
+interface Props {
+  message: string;
+  sender: string;
+}
+
+function ChatMessage({ message, sender }: Props) {
+  return (
+    <div>
+      {sender === "robot" && <img src={RobotProfileImage} width={40} />}
+      {message}
+      {sender === "user" && <img src={userProfileImage} width={40} />}
+    </div>
+  );
+}
+
+export default ChatMessage;
