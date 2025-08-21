6427708bf18702f4199061be69190c4e0d253328
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-08-21 11:54:04 +0500

    Create ChatInput Component

---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,10 @@
+const ChatInput = () => {
+  return (
+    <>
+      <input placeholder="Send a message to chatbot" size={30} />
+      <button>Send</button>
+    </>
+  );
+};
+
+export default ChatInput;
