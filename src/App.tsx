0146db2a76c808b1ed51a98c1adec4f616c7dc0e
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-08-23 12:24:12 +0500

    Add new message to a chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ChatInput from "./components/ChatInput";
 import { ChatMessages } from "./components/ChatMessages";
 import type { ChatMessageType } from "./types/chat";
+import { Chatbot } from "supersimpledev";
 
 const App = () => {
   const [chatMessages, setChatMessages] = useState<ChatMessageType[]>([]);
+
+  useEffect(() => {
+    Chatbot.addResponses({
+      goodbye: "Goodbye. Have a great day!",
+      "give me a unique id": function () {
+        return `Sure! Here's a unique ID: ${crypto.randomUUID()}`;
+      },
+    });
+
+    // [] tells useEffect to only run once. We only want to run
+    // this setup code once because we only want to add these
+    // extra responses once.
+  }, []);
 
   return (
     <div className="max-w-[600px] ml-auto mr-auto h-screen flex flex-col">
